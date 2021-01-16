0154f69b1d57dbdc99e1598da9b62460e3a5eea9
askinsaglam <saglamaskin@gmail.com>
2021-01-16 16:31:34 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs.js');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+
+    const app = express();
+    server.applyMiddleware({ app });
+    return app.listen({ port: 4000 });
+  })
+  .then((res) => {
+    console.log('Now browse to http://localhost:4000' + server.graphqlPath);
+  });
