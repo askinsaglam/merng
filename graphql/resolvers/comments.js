820ad1c2018389de151844307ebddb61e6846bfe
askinsaglam <saglamaskin@gmail.com>
2021-03-07 17:18:52 +0300

    Deleting on a specific post's comment has been added

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server-express'); 
+const { AuthenticationError, UserInputError } = require('apollo-server-express'); 
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/authentication');
@@ -32,6 +32,27 @@ module.exports = {
         } else {
           throw new UserInputError('Post not found');
         }
+    },
+    deleteComment: async (parent, args, context, info) => {
+       const { username } = checkAuth(context);
+       
+       const { postId, commentId } = args;
+       const post = await Post.findById(postId);
+
+       if(post) {
+         const commentIndex = post.comments.findIndex(c => c.id === commentId);
+         
+         if(post.comments[commentIndex].username === username){
+           post.comments.splice(commentIndex, 1);
+           await post.save();
+
+           return post;
+         } else {
+           throw new AuthenticationError('Action not allowed');
+         }
+       } else {
+         throw new UserInputError('Post not found');
+       }
     }
   }
 }
