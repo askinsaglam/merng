4f01bc2ed6865d0b460b6a998c1833066b901b7a
askinsaglam <saglamaskin@gmail.com>
2021-03-07 18:02:53 +0300

    likePost has been added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError, UserInputError } = require('apollo-server-express');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/authentication');
@@ -63,6 +63,32 @@ module.exports = {
         }
       } catch (err) {
         throw new Error(err);
+      }
+    },
+    async likePost(parent, args, context, info) {
+      const { username } = checkAuth(context);
+      
+      const { postId } = args;
+
+      const post = await Post.findById(postId);
+      
+      if(post){
+        if(post.likes.find(like => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);
+      
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          })
+        }
+
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError('Post not found')
       }
     }
   }
