0d32a26c3f600a991fb77153e5136b9b1778b721
askinsaglam <saglamaskin@gmail.com>
2021-04-18 23:05:10 +0300

    Client project has been set up

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hi WORLD!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
